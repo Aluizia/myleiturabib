d85d71679bc344f5d0e15923309d1abf31274947
Aluizia <95940476+Aluizia@users.noreply.github.com>
2026-01-11 16:12:08 -0300

    Delete bot color handling code
    Removed bot color management functionality.

--- main.js
+++ main.js
@@ -23,25 +23,3 @@
   });
 
 
- const bott = document.querySelectorAll(".bot");
-
-  bott.forEach((bot, index) => {
-    // restaura estado salvo
-    const corSalva = localStorage.getItem("bot_" + index);
-    if (corSalva) {
-      botao.style.backgroundColor = corSalva;
-    }
-
-    bot.addEventListener("click", () => {
-      const corAtual = bot.style.backgroundColor;
-      const novaCor = bot.dataset.cor;
-
-      if (corAtual === novaCor) {
-        bot.style.backgroundColor = "gray";
-        localStorage.removeItem("bot_" + index);
-      } else {
-        bot.style.backgroundColor = novaCor;
-        localStorage.setItem("bot_" + index, novaCor);
-      }
-    });
-  });
